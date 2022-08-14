c48e20006d909f9e310c4835df2009859e970a70
John Fairhurst <johnfairh@gmail.com>
2022-08-14 11:47:38 +0100

    Put something on the screen

--- Sources/MetalEngine/Renderer.swift
+++ Sources/MetalEngine/Renderer.swift
@@ -45,11 +45,12 @@ class Renderer: NSObject, Engine, MTKViewDelegate {
 
         buildPipelines()
 
+        bufferSetup()
         clientSetup(self)
     }
 
     public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        viewportSize = size // XXX need to decide if this should be points or pixels
+        viewportSize = size // XXX need to decide if this should be points or pixels YYY has to be points
     }
 
     func buildPipelines() {
@@ -62,23 +63,17 @@ class Renderer: NSObject, Engine, MTKViewDelegate {
         guard let library = try? metalDevice.makeDefaultLibrary(bundle: bundle) else {
             preconditionFailure("Can't load metal shader library")
         }
-        let functionNames = [
-            "vertex_passthrough",
-            "fragment_passthrough"
-        ]
-        let functions = Dictionary<String, MTLFunction>(
-            uniqueKeysWithValues: functionNames.map { name in
-                (name, library.makeFunction(name: name)!)
-            }
-        )
 
-        let passthroughDescriptor = MTLRenderPipelineDescriptor()
-        passthroughDescriptor.label = "Passthrough"
-        passthroughDescriptor.vertexFunction = functions["vertex_passthrough"]
-        passthroughDescriptor.fragmentFunction = functions["fragment_passthrough"]
-        passthroughDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        func makePipeline(_ label: String, _ vertex: String, _ fragment: String) -> MTLRenderPipelineState {
+            let passthroughDescriptor = MTLRenderPipelineDescriptor()
+            passthroughDescriptor.label = label
+            passthroughDescriptor.vertexFunction = library.makeFunction(name: vertex)!
+            passthroughDescriptor.fragmentFunction = library.makeFunction(name: fragment)!
+            passthroughDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+            return try! metalDevice.makeRenderPipelineState(descriptor: passthroughDescriptor)
 
-        passthroughPipeline = try! metalDevice.makeRenderPipelineState(descriptor: passthroughDescriptor)
+        }
+        passthroughPipeline = makePipeline("Passthrough", "vertex_passthrough", "fragment_passthrough")
     }
 
     public func draw(in view: MTKView) {
@@ -94,8 +89,43 @@ class Renderer: NSObject, Engine, MTKViewDelegate {
             return
         }
         clientFrame(self)
+        bufferRender(encoder: encoder)
         encoder.endEncoding()
         commandBuffer.present(drawable)
         commandBuffer.commit()
+    }
+
+    let positionPoints: [Float] = [
+        0.0,  0.5, 0, 1,
+        -0.5, -0.5, 0, 1,
+        0.5, -0.5, 0, 1,
+        -0.9, -0.9, 0, 1,
+        -0.7, -0.9, 0, 1,
+        -0.8, -0.7, 0, 1
+    ]
+
+    var positionBuffer: MTLBuffer!
+
+    let colourValues: [Float] = [
+        1, 0, 0, 1,
+        0, 1, 0, 1,
+        0, 0, 1, 1,
+        1, 0, 0, 1,
+        0, 1, 0, 1,
+        0, 0, 1, 1,
+    ]
+
+    var colourBuffer: MTLBuffer!
+
+    func bufferSetup() {
+        positionBuffer = metalDevice.makeBuffer(bytes: positionPoints, length: MemoryLayout<Float>.stride * positionPoints.count)
+        colourBuffer = metalDevice.makeBuffer(bytes: colourValues, length: MemoryLayout<Float>.stride * colourValues.count)
+    }
+
+    func bufferRender(encoder: MTLRenderCommandEncoder) {
+        encoder.setRenderPipelineState(passthroughPipeline)
+        encoder.setVertexBuffer(positionBuffer, offset: 0, index: 0)
+        encoder.setVertexBuffer(colourBuffer, offset: 0, index: 1)
+        encoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 6, instanceCount: 1)
     }
 }
